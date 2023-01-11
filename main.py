1eea6b4ad4ebfead024efed2195de9db69384edc
Suyuan Chen <suyuan.chen@itsp-inc.com>
2023-01-11 16:48:06 -0500

    half way

--- main.py
+++ main.py
@@ -80,12 +80,29 @@ def add_post():
             db.session.add(new_post)
             db.session.commit()
         return redirect(url_for("get_all_posts"))
-    return render_template("make-post.html", form=form)
+    return render_template("make-post.html", form=form, edit=False)
 
 
-@app.route("/new-post", methods=["POST"])
-def post():
-    pass
+@app.route("/edit/<int:post_id>", methods=["GET", "POST"])
+def edit_post(post_id):
+    post = BlogPost.query.get(post_id)
+    edit_form = CreatePostForm(title=post.title,
+        subtitle=post.subtitle,
+        img_url=post.img_url,
+        author=post.author,
+        body=post.body)
+    with app.app_context():
+        if edit_form.validate_on_submit():
+            post.title = edit_form.title.data
+            post.subtitle = edit_form.subtitle.data
+            post.img_url = edit_form.img_url.data
+            post.author = edit_form.author.data
+            post.body = edit_form.body.data
+            db.session.commit()
+            return render_template("post.html", post=post)
+    return render_template("make-post.html", form=edit_form, edit=True)
+
+
 
 
 
